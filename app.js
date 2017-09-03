c2a3afd73ecb8ea1caf83570f557f4e102f4ba43
Tarun K. Mall <c2tarun@hotmail.com>
2017-09-03 15:21:19 -0700

    Error handling and game setup complete

--- app.js
+++ app.js
@@ -5,24 +5,31 @@
 
     app.controller('SetupGameController', ['$scope', function($scope){
         $scope._playersList = [];
+        $scope._gameStarted = false;
+
         this.initialBalance = 1500;
-        this.newPlayerme = '';
-        this.gameStarted = false;
+        this.newPlayerName = '';
+        this.errorMessage = '';
 
         console.log(this.initialBalance);
         this.addPlayer = function() {
             var players = $scope._playersList;
-            var player = {
-                name: this.newPlayerName,
-                balance: 1500
-            };
-            players.push(player);
-            console.log(players);
+            this.errorMessage = '';
+            if(this.newPlayerName) {
+                var player = {
+                    name: this.newPlayerName,
+                    balance: 1500
+                };
+                players.push(player);
+            } else {
+                this.errorMessage = "Name cannot be empty";
+            }
+            
             this.newPlayerName = '';
         };
 
         this.startGame = function() {
-            this.gameStarted = true;
+            $scope._gameStarted = true;
         };
     }]);
 
